1b8c40f157354dbdac8961ac922513bba1fc2a8e
Pavel Uvarov <puvar@puvar.net>
2017-11-24 14:02:57 -0800

    hfmt

--- Cat.hs
+++ Cat.hs
@@ -1,32 +1,34 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad
-import Data.Int
-import Data.List
-import System.Console.GetOpt
-import System.Environment
-import System.IO
-import Text.Printf
-import qualified Data.ByteString.Builder as B
+import           Control.Applicative
+import           Control.Monad
+import qualified Data.ByteString.Builder    as B
+import qualified Data.ByteString.Char8      as S
 import qualified Data.ByteString.Lazy.Char8 as L
-import qualified Data.ByteString.Char8 as S
+import           Data.Int
+import           Data.List
+import           System.Console.GetOpt
+import           System.Environment
+import           System.IO
+import           Text.Printf
 
-data Flag = N deriving Eq
+data Flag =
+  N
+  deriving (Eq)
 
 getOptions :: IO ([Flag], [String])
 getOptions = do
   args <- getArgs
   case getOpt RequireOrder options args of
     (flags, filepaths, []) -> return (flags, filepaths)
-    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    (_, _, errs) ->
+      ioError (userError (concat errs ++ usageInfo header options))
   where
     header = "Usage: cat [OPTION]... [FILE]..."
-    options =
-      [ Option "n" ["number"] (NoArg N) "number all output lines"
-      ]
+    options = [Option "n" ["number"] (NoArg N) "number all output lines"]
 
 infixr 4 <>
+
 (<>) :: Monoid m => m -> m -> m
 (<>) = mappend
 
@@ -35,16 +37,21 @@ catNumbered s =
   (\(_, builder) -> B.hPutBuilder stdout builder) $
   L.foldl
     (\((n, newline), builder) ch ->
-      ( case ch of
-          '\n' -> (n + 1, True)
-          _ -> (n, False)
-      , case newline of
-          True ->
-            let spaces = L.replicate (max 0 $ 6 - intLog10 n) ' ' in
-            foldl (<>) builder [B.lazyByteString spaces, B.int64Dec n, B.char8 '\t', B.char8 ch]
-          False ->
-            builder <> B.char8 ch)
-    )
+       ( case ch of
+           '\n' -> (n + 1, True)
+           _    -> (n, False)
+       , case newline of
+           True ->
+             let spaces = L.replicate (max 0 $ 6 - intLog10 n) ' '
+             in foldl
+                  (<>)
+                  builder
+                  [ B.lazyByteString spaces
+                  , B.int64Dec n
+                  , B.char8 '\t'
+                  , B.char8 ch
+                  ]
+           False -> builder <> B.char8 ch))
     ((1 :: Int64, True), mempty)
     s
   where
@@ -55,18 +62,19 @@ catNumbered s =
 main :: IO ()
 main = do
   (flags, filepaths') <- getOptions
-  let filepaths = if null filepaths' then ["-"] else filepaths'
-
+  let filepaths =
+        if null filepaths'
+          then ["-"]
+          else filepaths'
 --  let filters = [filterN']
 --  let cat = \s -> putStr $ foldl (\s f -> f s) s filters
-  let cat = if elem N flags then catNumbered else L.putStr
-  
-  forM_ filepaths
+  let cat =
+        if elem N flags
+          then catNumbered
+          else L.putStr
+  forM_
+    filepaths
     (\filepath ->
        case filepath of
-         "-" ->
-           cat =<< L.getContents
-         filename ->
-           cat =<< L.readFile filename)
-
-    
+         "-"      -> cat =<< L.getContents
+         filename -> cat =<< L.readFile filename)
